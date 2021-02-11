1f58f282dcd0fdf3b7697bce9f77d0f3ec96060d
ltsuda <leonardo.tsuda@icloud.com>
2021-02-11 01:08:58 -0300

    add inventory page tests

---
+++ saucedemo/resources/locators/inventoryLocators.js
@@ -0,0 +1,31 @@
+const INVENTORY_HEADER = "[#inventory_filter_container >> .product_label]"
+const INVENTORY_CART = "[class='shopping_cart_link']"
+const INVENTORY_CART_BADGE = "[class='fa-layers-counter shopping_cart_badge']"
+const INVENTORY_SORT = "[class=product_sort_container]"
+const PRODUCT_LIST = "[class='inventory_item']"
+const PRODUCT_IMG = "[.inventory_item >> .inventory_item_img >> img]"
+const PRODUCT_NAME = "[class='inventory_item_name']"
+const PRODUCT_DESCRIPTION = "[class='inventory_item_desc']"
+const PRODUCT_PRICE = "[class='inventory_item_price']"
+const BTN_ADD_TO_CART = "[class='btn_primary btn_inventory']"
+const FOOTER_SOCIALS = "[footer >> .social]"
+const FOOTER_TWITTER = "[footer >> .social_twitter >> text=Twitter]"
+const FOOTER_FACEBOOK = "[footer >> .social_facebook >> text=Facebook]"
+const FOOTER_LINKEDIN = "[footer >> .social_linkedin >> text=LinkedIn]"
+
+module.exports = {
+  INVENTORY_HEADER,
+  INVENTORY_CART,
+  INVENTORY_CART_BADGE,
+  INVENTORY_SORT,
+  PRODUCT_LIST,
+  PRODUCT_IMG,
+  PRODUCT_NAME,
+  PRODUCT_DESCRIPTION,
+  PRODUCT_PRICE,
+  BTN_ADD_TO_CART,
+  FOOTER_SOCIALS,
+  FOOTER_TWITTER,
+  FOOTER_FACEBOOK,
+  FOOTER_LINKEDIN,
+}
---
+++ test/saucedemo/inventory.spec.js
@@ -0,0 +1,68 @@
+const inventoryLocators = require("../../saucedemo/resources/locators/inventoryLocators")
+const { chromium } = require("playwright")
+const { LoginPage } = require("../../saucedemo/models/LoginPage")
+const { InventoryPage } = require("../../saucedemo/models/InventoryPage")
+const { expect } = require("chai")
+
+let browser, context, page, loginPage
+
+describe("Sauce inventory demo", () => {
+  before(async () => {
+    browser = await chromium.launch()
+  })
+
+  after(async () => {
+    await browser.close()
+  })
+
+  beforeEach(async () => {
+    context = await browser.newContext()
+    page = await context.newPage()
+    loggedPage = new LoginPage(page)
+    await loggedPage.navigate()
+    await loggedPage.loginWithStandardUser()
+    await page.waitForLoadState()
+    inventoryPage = new InventoryPage(page)
+  })
+
+  afterEach(async () => {
+    await context.close()
+  })
+
+  it("should be at Inventory page", async () => {
+    expect(await page.url()).to.be.equal(process.env.SAUCE_INVENTORY_URL)
+  })
+
+  it("shows a list of items", async () => {
+    productCount = await inventoryPage.getProductsCount()
+    expect(productCount).greaterThan(0)
+  })
+
+  it("should be sorted from A-Z", async () => {
+    productNameList = await inventoryPage.getProductsName()
+    expect(productNameList).be.equal(productNameList.sort())
+  })
+
+  it("could sort product list from Z-A", async () => {
+    await inventoryPage.sortProductList(inventoryPage.sortOptions.Z_A)
+    productNameList = await inventoryPage.getProductsName()
+    expect(productNameList).be.equal(productNameList.reverse())
+  })
+
+  it("could sort product list from Lower to Higher value", async () => {
+    await inventoryPage.sortProductList(inventoryPage.sortOptions.LowHigh)
+    productPriceList = await inventoryPage.getProductsPrice()
+    expect(productPriceList).be.equal(productPriceList.sort())
+  })
+
+  it("could sort product list from Higher to Lower value", async () => {
+    await inventoryPage.sortProductList(inventoryPage.sortOptions.HighLow)
+    productPriceList = await inventoryPage.getProductsPrice()
+    expect(productPriceList).be.equal(productPriceList.reverse())
+  })
+
+  it("shows cart badge number", async () => {
+    await inventoryPage.addFirstProductToCart()
+    expect(await inventoryPage.getCartBadge()).to.be.equal("1")
+  })
+})
