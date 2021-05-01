9a23f8dc5423cdd3e90fb99294f08c39a72f86f6
ltsuda <leonardo.tsuda@icloud.com>
2021-04-30 22:09:45 -0300

    add randomInt and function to login and save cookies file

--- saucedemo/utils/utils.js
+++ saucedemo/utils/utils.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const { LoginController } = require("../../saucedemo/pages/login/controller")
 
 async function saveCookies(context) {
   const cookies = await context.cookies()
@@ -12,7 +13,23 @@ async function loadCookies(context) {
   await context.addCookies(deserializedCookies)
 }
 
+async function loginAndSaveCookies(browser) {
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  const loginController = new LoginController(page)
+  await loginController.navigate()
+  await loginController.loginWithStandardUser()
+  await saveCookies(context)
+  await context.close()
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * max)
+}
+
 module.exports = {
+  loginAndSaveCookies,
   saveCookies,
   loadCookies,
+  randomInt,
 }
