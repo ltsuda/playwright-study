fc2de88aaaa152f0760b8c3c3c92f43bbbf5c5ba
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-12 16:09:52 -0300

    add jsdoc to cart files

--- saucedemo/pages/cart/components.js
+++ saucedemo/pages/cart/components.js
@@ -1,22 +1,50 @@
+/* eslint-disable no-unused-vars */
+const { Page, ElementHandle } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
+
+/**
+ * Object representing Cart's HTML selectors
+ */
 const cartLocators = {
     cartContainer: ".cart_contents_container",
     continueShoppingButton: "[data-test='continue-shopping']",
     checkoutButton: "[data-test='checkout']",
 }
 
+/**
+ * Class representing playwright's ElementHandles from /cart.html page\
+ * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ */
 class CartComponents {
+    /**
+     * Create the Cart ElementsHandle
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     */
     constructor(page) {
         this.page = page
     }
 
+    /**
+     * Get the cart's container ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'cartContainer' locator
+     */
     async cartContainer() {
         return await this.page.$(cartLocators.cartContainer)
     }
 
+    /**
+     * Get the cart's container ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'continueShoppingButton' locator
+     */
     async continueShoppingButton() {
         return await this.page.$(cartLocators.continueShoppingButton)
     }
 
+    /**
+     * Get the cart's container ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'checkoutButton' locator
+     */
     async checkoutButton() {
         return await this.page.$(cartLocators.checkoutButton)
     }
--- saucedemo/pages/cart/controller.js
+++ saucedemo/pages/cart/controller.js
@@ -1,27 +1,53 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { CartComponents, cartLocators } = require("./components")
 const { PAGES } = require("../../utils/consts")
 
+
+/**
+ * Class representing the Cart's page interations
+ */
 class CartController {
+    /**
+     * Create the Cart controller
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     * @param {CartComponents} components - class with elementsHandle of the Cart page
+     * @param {Object} locators - page's selectors
+     */
     constructor(page) {
         this.page = page
         this.components = new CartComponents(this.page)
         this.locators = cartLocators
     }
 
+    /**
+     * Navigate to the Cart page and wait until network is idle
+     */
     async navigate() {
         await this.page.goto(PAGES.CART, "networkidle")
     }
 
+    /**
+     * Validate if "cartContainer" element is visible
+     */
     async screenIsVisible() {
         const cartContainerElement = await this.components.cartContainer()
         await cartContainerElement.isVisible()
     }
 
+    /**
+     * Click at the "Continue Shopping" button
+     */
     async continueShopping() {
         const continueButton = await this.components.continueShoppingButton()
         await continueButton.click()
     }
 
+    /**
+     * Click at the "Checkout" button
+     */
     async navigateToCheckout() {
         const checkoutButton = await this.components.checkoutButton()
         await checkoutButton.click()
