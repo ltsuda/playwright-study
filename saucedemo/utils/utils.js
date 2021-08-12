c495719580bcc8dd2d727ade4df89df4f3e5184b
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-12 15:11:57 -0300

    add jsdoc comments to utils functions

--- saucedemo/utils/utils.js
+++ saucedemo/utils/utils.js
@@ -1,9 +1,27 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { PAGES } = require("./consts")
 
+/**
+ * Choose a random integer between 0 and @max
+ * @param {number} max - the high limit for the random number
+ * @returns {number} the random number generated
+ */
 function randomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
+/**
+ * Set browser's cookies and localStore directly using the document object
+ * and navigate to the destination URL path
+ * @param {Page} page - playwright browser's page\
+ * See {@link https://playwright.dev/docs/api/class-page}
+ * @param {Object} data - data comming from test suite or test cases
+ * @param {String} data.path - url destination path
+ * @param {number[]} [data.products=[]] - list of products index
+ * @param {String} data.username - username credentials to authenticate on website
+ */
 async function setSession(page, data = {}) {
     const { BASEURL } = PAGES
     const { path, products = [], username = "" } = data
