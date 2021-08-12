73263abf1565542639905fc2bdd165036cb77248
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-12 16:49:07 -0300

    add jsdoc to footer files

--- saucedemo/pages/footer/components.js
+++ saucedemo/pages/footer/components.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-unused-vars */
+const { Page, ElementHandle } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
+
+/**
+ * Object representing Footer's HTML selectors
+ */
 const footerLocators = {
     footerContainer: ".footer",
     twitterSocialLink: ".footer >> .social_twitter > a",
@@ -7,31 +14,65 @@ const footerLocators = {
     footerImage: ".footer >> .footer_robot",
 }
 
+/**
+ * Class representing playwright's ElementHandles for the footer elements\
+ * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ */
 class FooterComponents {
+
+    /**
+     * Create the Footer ElementsHandle
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     */
     constructor(page) {
         this.page = page
     }
 
+    /**
+     * Get the footer's container ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'footerContainer' locator
+     */
     async footerContainer() {
         return await this.page.$(footerLocators.footerContainer)
     }
 
+    /**
+     * Get the footer's header text ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'completedHeaderText' locator
+     */
     async twitterLink() {
         return await this.page.$(footerLocators.twitterSocialLink)
     }
 
+    /**
+     * Get the footer's facebook link ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'facebookSocialLink' locator
+     */
     async facebookLink() {
         return await this.page.$(footerLocators.facebookSocialLink)
     }
 
+    /**
+     * Get the footer's linkedin link ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'linkdinSocialLink' locator
+     */
     async linkedinLink() {
         return await this.page.$(footerLocators.linkdinSocialLink)
     }
 
+    /**
+     * Get the footer's copyright text ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'copyrightText' locator
+     */
     async copyrightText() {
         return await this.page.$(footerLocators.copyrightText)
     }
 
+    /**
+     * Get the footer's robot image ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'footerImage' locator
+     */
     async robotImage() {
         return await this.page.$(footerLocators.footerImage)
     }
--- saucedemo/pages/footer/controller.js
+++ saucedemo/pages/footer/controller.js
@@ -1,37 +1,73 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { FooterComponents, footerLocators } = require("./components")
 
+/**
+ * Class representing the Footer's elements interations
+ */
 class FooterController {
+    /**
+     * Create the Footer controller
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     * @param {CartComponents} components - class with elementsHandle of the footer elements
+     * @param {Object} locators - page's selectors
+     */
     constructor(page) {
         this.page = page
         this.components = new FooterComponents(this.page)
         this.locators = footerLocators
     }
 
+    /**
+     * Validate if "footerContainer" element is visible
+     */
     async componentIsVisible() {
         const footerContainerElement = await this.components.footerContainer()
         await footerContainerElement.isVisible()
     }
 
+    /**
+     * Get Twitter link
+     * @returns href Twitter's URL
+     */
     async getTwitterLink() {
         const socialElement = await this.components.twitterLink()
         return await socialElement.getAttribute("href")
     }
 
+    /**
+     * Get Facebook link
+     * @returns href Facebook's URL
+     */
     async getFacebookLink() {
         const socialElement = await this.components.facebookLink()
         return await socialElement.getAttribute("href")
     }
 
+    /**
+     * Get LinkedIn link
+     * @returns href LinkedIn's URL
+     */
     async getLinkedinLink() {
         const socialElement = await this.components.linkedinLink()
         return await socialElement.getAttribute("href")
     }
 
+    /**
+     * Get Robot image source
+     * @returns image's source URL
+     */
     async getRobotImage() {
         const socialElement = await this.components.robotImage()
         return await socialElement.getAttribute("src")
     }
 
+    /**
+     * Get copyright text
+     * @returns copyright's element text
+     */
     async getCopyrightText() {
         const socialElement = await this.components.copyrightText()
         return await socialElement.innerText()
