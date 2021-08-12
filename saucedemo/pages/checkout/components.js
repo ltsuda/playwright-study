2a64cc28bd5278f5ef1e59137c9b8ffd41db9cd6
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-12 16:24:07 -0300

    add jsdoc to checkout files

--- saucedemo/pages/checkout/components.js
+++ saucedemo/pages/checkout/components.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-unused-vars */
+const { Page, ElementHandle } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
+
+/**
+ * Object representing Checkout's HTML selectors
+ */
 const checkoutLocators = {
     checkoutContainer: ".checkout_info_container",
     checkoutForm: "[class='checkout_info']",
@@ -9,35 +16,72 @@ const checkoutLocators = {
     errorMessageText: "[data-test='error']",
 }
 
+/**
+ * Class representing playwright's ElementHandles from /checkout.html page\
+ * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ */
 class CheckoutComponents {
+    /**
+     * Create the Checkout ElementsHandle
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     */
     constructor(page) {
         this.page = page
     }
 
+    /**
+     * Get the checkout's container ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'checkoutContainer' locator
+     */
     async checkoutContainer() {
         return await this.page.$(checkoutLocators.checkoutContainer)
     }
 
+    /**
+     * Get the checkout's cancel button ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'cancelButton' locator
+     */
     async cancelButton() {
         return await this.page.$(checkoutLocators.cancelButton)
     }
 
+    /**
+     * Get the checkout's continue button ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'continueButton' locator
+     */
     async continueButton() {
         return await this.page.$(checkoutLocators.continueButton)
     }
 
+    /**
+     * Get the checkout's error message text ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'errorMessageText' locator
+     */
     async errorMessageText() {
         return this.page.$(checkoutLocators.errorMessageText)
     }
 
+    /**
+     * Get the checkout's first name input ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'firstNameInput' locator
+     */
     async firstNameInput() {
         return this.page.$(`${checkoutLocators.checkoutForm}` + ">>" + `${checkoutLocators.firstNameInput}`)
     }
 
+    /**
+     * Get the checkout's last name input ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'lastNameInput' locator
+     */
     async lastNameInput() {
         return this.page.$(`${checkoutLocators.checkoutForm}` + ">>" + `${checkoutLocators.lastNameInput}`)
     }
 
+    /**
+     * Get the checkout's postal code input ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'postalCodeInput' locator
+     */
     async postalCodeInput() {
         return this.page.$(`${checkoutLocators.checkoutForm}` + ">>" + `${checkoutLocators.postalCodeInput}`)
     }
--- saucedemo/pages/checkout/controller.js
+++ saucedemo/pages/checkout/controller.js
@@ -1,52 +1,93 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { CheckoutComponents, checkoutLocators } = require("./components")
 const { PAGES, PERSONAL_INFO } = require("../../utils/consts")
 
+/**
+ * Class representing the Checkout's page interations
+ */
 class CheckoutController {
+    /**
+     * Create the Checkout controller
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     * @param {CheckoutComponents} components - class with elementsHandle of the Checkout page
+     * @param {Object} locators - page's selectors
+     */
     constructor(page) {
         this.page = page
         this.components = new CheckoutComponents(this.page)
         this.locators = checkoutLocators
     }
 
+    /**
+     * Navigate to the Checkout page and wait until network is idle
+     */
     async navigate() {
         await this.page.goto(PAGES.CHECKOUT, "networkidle")
     }
 
+    /**
+     * Validate if "checkoutContainer" element is visible
+     */
     async screenIsVisible() {
         const checkoutContainerElement = await this.components.checkoutContainer()
         await checkoutContainerElement.isVisible()
     }
 
+    /**
+     * Click at the "Cancel checkout" button
+     */
     async cancelCheckout() {
         const cancelElement = await this.components.cancelButton()
         await cancelElement.click()
     }
 
+    /**
+     * Click at the "Continue" button
+     */
     async continueCheckout() {
         const continueElement = await this.components.continueButton()
         await continueElement.click()
     }
 
+    /**
+     * Get error message text
+     * @returns checkout's error message
+     */
     async getErrorMessage() {
         const errorElement = await this.components.errorMessageText()
         return await errorElement.innerText()
     }
 
+    /**
+     * Fill up First name input
+     */
     async fillFirstName(firstName) {
         const firstNameElement = await this.components.firstNameInput()
         await firstNameElement.fill(firstName)
     }
 
+    /**
+     * Fill up Last name input
+     */
     async fillLastName(lastName) {
         const lastNameElement = await this.components.lastNameInput()
         await lastNameElement.fill(lastName)
     }
 
+    /**
+     * Fill up Postal code input
+     */
     async fillPostalCode(postalCode) {
         const postalCodeElement = await this.components.postalCodeInput()
         await postalCodeElement.fill(postalCode)
     }
 
+    /**
+     * Fill up first name, last name and postal code inputs and click at the "Continue" button
+     */
     async submitCheckout(
         firstName = PERSONAL_INFO.USER1.FIRST_NAME,
         lastName = PERSONAL_INFO.USER1.LAST_NAME,
