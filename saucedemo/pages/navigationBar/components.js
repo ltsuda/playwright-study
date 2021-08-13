933a1b85696348ac707dc59687e7a0a9dffcfbe0
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-13 00:25:11 -0300

    add jsdoc to navigationbar files

--- saucedemo/pages/navigationBar/components.js
+++ saucedemo/pages/navigationBar/components.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-unused-vars */
+const { Page, ElementHandle } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
+
+/**
+ * Object representing Navigationbar's HTML selectors
+ */
 const navigationBarLocators = {
     navContainer: ".header_container",
     navDetailContainer: ".header_container.inventory_details",
@@ -12,51 +19,104 @@ const navigationBarLocators = {
     cartBadgeText: "[class='shopping_cart_badge']",
 }
 
+/**
+ * Class representing playwright's ElementHandles for the navigationbar elements\
+ * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ */
 class NavigationBarComponents {
+    /**
+     * Create the Footer ElementsHandle
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     */
     constructor(page) {
         this.page = page
     }
 
+    /**
+     * Get the navigationbar's container ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'navContainer' locator
+     */
     async navContainer() {
         return await this.page.$(navigationBarLocators.navContainer)
     }
 
+    /**
+     * Get the naviationbar detail's container ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'navDetailContainer' locator
+     */
     async navDetailContainer() {
         return await this.page.$(navigationBarLocators.navDetailContainer)
     }
 
+    /**
+     * Get the sidemenu's container ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'sideMenu' locator
+     */
     async sideMenu() {
         return await this.page.$(navigationBarLocators.sideMenu)
     }
 
+    /**
+     * Get the sidemenu button ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'menuButton' locator
+     */
     async menuButton() {
         return await this.page.$(navigationBarLocators.menuButton)
     }
 
+    /**
+     * Get the close menu button ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'menuCloseButton' locator
+     */
     async menuCloseButton() {
         return await this.page.$(navigationBarLocators.menuCloseButton)
     }
 
+    /**
+     * Get the all items link ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'menuAllItemsLink' locator
+     */
     async menuAllItemsLink() {
         return await this.page.$(navigationBarLocators.menuAllItemsLink)
     }
 
+    /**
+     * Get the about link ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'menuAboutLink' locator
+     */
     async menuAboutLink() {
         return await this.page.$(navigationBarLocators.menuAboutLink)
     }
 
+    /**
+     * Get the logout link ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'menuLogoutLink' locator
+     */
     async menuLogoutLink() {
         return await this.page.$(navigationBarLocators.menuLogoutLink)
     }
 
+    /**
+     * Get the reset state link ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'menuResetStateLink' locator
+     */
     async menuResetStateLink() {
         return await this.page.$(navigationBarLocators.menuResetStateLink)
     }
 
+    /**
+     * Get the cart icon link ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'cartLink' locator
+     */
     async cartLink() {
         return await this.page.$(navigationBarLocators.cartLink)
     }
 
+    /**
+     * Get the cart badge ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'cartBadgeText' locator
+     */
     async cartBadgeText() {
         return await this.page.$(`${navigationBarLocators.cartLink}` + ">" + `${navigationBarLocators.cartBadgeText}`)
     }
--- saucedemo/pages/navigationBar/controller.js
+++ saucedemo/pages/navigationBar/controller.js
@@ -1,52 +1,89 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { NavigationBarComponents, navigationBarLocators } = require("./components")
 
+/**
+ * Object representing Navigationbar's HTML selectors
+ */
 class NavigationBarController {
+    /**
+     * Class representing playwright's ElementHandles for the navigationbar elements\
+     * See {@link https://playwright.dev/docs/api/class-elementhandle}
+     */
     constructor(page) {
         this.page = page
         this.components = new NavigationBarComponents(this.page)
         this.locators = navigationBarLocators
     }
 
+    /**
+     * Validate if "navContainer" element is visible
+     */
     async componentIsVisible() {
         const navContainerElement = await this.components.navContainer()
         await navContainerElement.isVisible()
     }
 
+    /**
+     * Validate if "navDetailContainer" element is visible
+     */
     async componenDetailIsVisible() {
         const navDetailContainerElement = await this.components.navDetailContainer()
         await navDetailContainerElement.isVisible()
     }
-
+    /**
+     * Click at the side menu button
+     */
     async openMenu() {
         const menuElement = await this.components.menuButton()
         await menuElement.click()
     }
 
+    /**
+     * Click at the close menu button
+     */
     async closeMenu() {
         const closeMenuElement = await this.components.menuCloseButton()
         await closeMenuElement.click()
     }
 
+    /**
+     * Click at the All Items link
+     */
     async allItems() {
         const allItemMenuElement = await this.components.menuAllItemsLink()
         await allItemMenuElement.click()
     }
 
+    /**
+     * Click at the About link
+     */
     async about() {
         const aboutElement = await this.components.menuAboutLink()
         await aboutElement.click()
     }
 
+    /**
+     * Click at the Logout link
+     */
     async logout() {
         const logoutMenuElement = await this.components.menuLogoutLink()
         await logoutMenuElement.click()
     }
 
+    /**
+     * Click at the Reset state link
+     */
     async resetState() {
         const resetMenuElement = await this.components.menuResetStateLink()
         await resetMenuElement.click()
     }
 
+    /**
+     * Get the cart badge text if element exists
+     * @returns {String|null} the number of items on the cart or null if the badge element doens't exist
+     */
     async getCartBadgeIfExists() {
         const badgeElement = await this.components.cartBadgeText()
         if ((await badgeElement) == null) {
@@ -55,11 +92,17 @@ class NavigationBarController {
         return await badgeElement.innerText()
     }
 
+    /**
+     * Click at the Cart icon link
+     */
     async navigateToCart() {
         const cartButtonElement = await this.components.cartLink()
         await cartButtonElement.click()
     }
 
+    /**
+     * Validate if sideMenu element is visible
+     */
     async isSidemenuVisible() {
         const sideMenuElement = await this.components.sideMenu()
         return await sideMenuElement.isVisible()
