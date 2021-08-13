f9172db5fbc2e9d4073ad09313968b016dcc13e1
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-12 22:52:29 -0300

    add jsdoc to inventory files

--- saucedemo/pages/inventory/components.js
+++ saucedemo/pages/inventory/components.js
@@ -1,12 +1,33 @@
+/* eslint-disable no-unused-vars */
+const { Page, ElementHandle } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
+
+/**
+ * Object representing Inventory's HTML selectors
+ */
 const inventoryLocators = {
     inventoryContainer: "#inventory_container",
 }
 
+/**
+ * Class representing playwright's ElementHandles from /inventory.html page\
+ * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ */
 class InventoryComponents {
+
+    /**
+     * Create the Inventory ElementsHandle
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     */
     constructor(page) {
         this.page = page
     }
 
+    /**
+     * Get the inventory's container ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'inventoryContainer' locator
+     */
     async inventoryContainer() {
         return await this.page.$(inventoryLocators.inventoryContainer)
     }
--- saucedemo/pages/inventory/controller.js
+++ saucedemo/pages/inventory/controller.js
@@ -1,16 +1,34 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { PAGES } = require("../../utils/consts")
 const { InventoryComponents } = require("./components")
 
+/**
+ * Class representing the Inventory's elements interations
+ */
 class InventoryController {
+    /**
+     * Create the Inventory controller
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     * @param {InventoryComponents} components - class with elementsHandle of the Inventory page
+     */
     constructor(page) {
         this.page = page
         this.components = new InventoryComponents(this.page)
     }
 
+    /**
+     * Navigate to the Inventory page and wait until network is idle
+     */
     async navigate() {
         await this.page.goto(PAGES.INVENTORY, "networkidle")
     }
 
+    /**
+     * Validate if "inventoryContainer" element is visible
+     */
     async screenIsVisible() {
         const inventoryContainerElement = await this.components.inventoryContainer()
         await inventoryContainerElement.isVisible()
