0c59cdbb189032f1eec27e13750b5c0672e22088
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-13 01:05:10 -0300

    add jsdoc to overview files

--- saucedemo/pages/overview/components.js
+++ saucedemo/pages/overview/components.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-unused-vars */
+const { Page, ElementHandle } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
+
+/**
+ * Object representing Overview's HTML selectors
+ */
 const overviewLocators = {
     overviewContainer: ".checkout_summary_container",
     cancelCheckoutButton: "[data-test='cancel']",
@@ -10,39 +17,80 @@ const overviewLocators = {
     totalPriceText: "[class='summary_total_label']",
 }
 
+/**
+ * Class representing playwright's ElementHandles from /checkout-two.html page\
+ * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ */
 class OverviewComponents {
+    /**
+     * Create the Pverview ElementsHandle
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     */
     constructor(page) {
         this.page = page
     }
 
+    /**
+     * Get the overview's container ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'overviewContainer' locator
+     */
     async overviewContainer() {
         return await this.page.$(overviewLocators.overviewContainer)
     }
 
+    /**
+     * Get the payment information text ElementHandle
+     * @returns {String} payment card element text
+     */
     async paymentInfoText() {
         return await this.page.$(`${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.paymentInfoText}`)
     }
 
+    /**
+     * Get the shipment information text ElementHandle
+     * @returns {String} shipment element text
+     */
     async shippingInfoText() {
         return await this.page.$(`${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.shippingInfoText}`)
     }
 
+    /**
+     * Get the subtotal text ElementHandle
+     * @returns {String} subtotal element text
+     */
     async subtotalText() {
         return await this.page.$(`${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.subtotalText}`)
     }
 
+    /**
+     * Get the tax text ElementHandle
+     * @returns {String} tax element text
+     */
     async taxText() {
         return await this.page.$(`${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.taxText}`)
     }
 
+    /**
+     * Get the total price text ElementHandle
+     * @returns {String} total price element text
+     */
     async totalPriceText() {
         return await this.page.$(`${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.totalPriceText}`)
     }
 
+    /**
+     * Get the cancel checkout button ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'cancelCheckoutButton' locator
+     */
     async cancelCheckoutButton() {
         return await this.page.$(overviewLocators.cancelCheckoutButton)
     }
 
+    /**
+     * Get the finish purchase button ElementHandle
+     * @returns {ElementHandle} ElementHandle for 'finishCheckoutButton' locator
+     */
     async finishCheckoutButton() {
         return await this.page.$(overviewLocators.finishCheckoutButton)
     }
--- saucedemo/pages/overview/controller.js
+++ saucedemo/pages/overview/controller.js
@@ -1,41 +1,78 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { OverviewComponents, overviewLocators } = require("./components")
 const { PAGES } = require("../../utils/consts")
 
+/**
+ * Class representing the Overview's page interations
+ */
 class OverviewController {
+    /**
+     * Create the Overview controller
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     * @param {CartComponents} components - class with elementsHandle of the Overview page
+     * @param {Object} locators - page's selectors
+     */
     constructor(page) {
         this.page = page
         this.components = new OverviewComponents(this.page)
         this.locators = overviewLocators
     }
 
+    /**
+     * Navigate to the Overview page and wait until network is idle
+     */
     async navigate() {
         await this.page.goto(PAGES.OVERVIEW, "networkidle")
     }
 
+    /**
+     * Validate if "overviewContainer" element is visible
+     */
     async screenIsVisible() {
         const overviewContainerElement = await this.components.overviewContainer()
         await overviewContainerElement.isVisible()
     }
 
+    /**
+     * Click at the "Cancel checkout" button
+     */
     async cancelCheckout() {
         const cancelCheckoutButton = await this.components.cancelCheckoutButton()
         await cancelCheckoutButton.click()
     }
 
+    /**
+     * Click at the "Finish" button
+     */
     async finishCheckout() {
         const finishCheckoutButton = await this.components.finishCheckoutButton()
         await finishCheckoutButton.click()
     }
 
+    /**
+     * Get the payment card information
+     * @returns {String} payment card element text
+     */
     async getPaymentText() {
         const paymentElement = await this.components.paymentInfoText()
         return await paymentElement.innerText()
     }
 
+    /**
+     * Get the shipment information
+     * @returns {String} shipment element text
+     */
     async getShippingText() {
         const shippingElement = await this.components.shippingInfoText()
         return await shippingElement.innerText()
-    }
+
+    /**
+     * Get subtotal value
+     * @returns {String} subtotal value element text
+     */}
     async getSubtotal() {
         const subtotalElement = await this.components.subtotalText()
         let subtotal = await subtotalElement.innerText()
@@ -43,6 +80,10 @@ class OverviewController {
         return parseFloat(subtotal.toFixed(2))
     }
 
+    /**
+     * Get tax value
+     * @returns {String} tax value element text
+     */
     async getTax() {
         const taxElement = await this.components.taxText()
         let tax = await taxElement.innerText()
@@ -50,6 +91,10 @@ class OverviewController {
         return parseFloat(tax.toFixed(2))
     }
 
+    /**
+     * Get total price value
+     * @returns {String} total value element text
+     */
     async getTotalPrice() {
         const priceElement = await this.components.totalPriceText()
         let total = await priceElement.innerText()
@@ -57,12 +102,20 @@ class OverviewController {
         return parseFloat(total.toFixed(2))
     }
 
+    /**
+     * Calculate subtotal by multiplying the subtotal by 80%
+     * @returns {String} calculated tax value
+     */
     async calculateTax() {
         const subTotal = await this.getSubtotal()
         const tax = (subTotal * 0.08).toFixed(2)
         return parseFloat(tax)
     }
 
+    /**
+     * Calculate total value by adding up the subtotal and the calculated tax value
+     * @returns {String} calculated total value
+     */
     async calculateTotal() {
         const subTotal = await this.getSubtotal()
         const tax = await this.calculateTax()
