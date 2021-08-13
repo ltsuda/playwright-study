992e60a1c836c46a38f4cb61ff71f5d9a7dd4b7c
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-12 23:53:45 -0300

    add jsdoc to login files

--- saucedemo/pages/login/components.js
+++ saucedemo/pages/login/components.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+const { Page, ElementHandle } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const loginLocators = {
     loginContainer: ".login_wrapper",
     credentialsContainer: ".login_credentials_wrap",
@@ -9,39 +12,79 @@ const loginLocators = {
     acceptedPasswordText: "[class='login_password']",
 }
 
+/**
+ * Object representing Login's HTML selectors
+ */
 class LoginComponents {
+    /**
+     * Create the Login ElementsHandle
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     */
     constructor(page) {
         this.page = page
     }
 
+    /**
+     * Get the Login's container ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'loginContainer' locator
+     */
     async loginContainer() {
         return await this.page.$(loginLocators.loginContainer)
     }
 
+    /**
+     * Get the credentials container's ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'credentialsContainer' locator
+     */
     async credentialsContainer() {
         return await this.page.$(loginLocators.credentialsContainer)
     }
 
+    /**
+     * Get the credentials text's ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'acceptedUsersText' locator
+     */
     async acceptedUsersText() {
         return await this.page.$(loginLocators.acceptedUsersText)
     }
 
+    /**
+     * Get the password text's ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'acceptedPasswordText' locator
+     */
     async acceptedPasswordText() {
         return await this.page.$(loginLocators.acceptedPasswordText)
     }
 
+    /**
+     * Get the error message text's ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'errorText' locator
+     */
     async errorMessageText() {
         return await this.page.$(loginLocators.errorText)
     }
 
+    /**
+     * Get the checkout's username input ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'usernameInput' locator
+     */
     async usernameInput() {
         return await this.page.$(loginLocators.usernameInput)
     }
 
+    /**
+     * Get the checkout's password input ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'passwordInput' locator
+     */
     async passwordInput() {
         return await this.page.$(loginLocators.passwordInput)
     }
 
+    /**
+     * Get the login button ElementHandle
+     * @returns {ElementHandle} - ElementHandle for 'loginButton' locator
+     */
     async loginButton() {
         return await this.page.$(loginLocators.loginButton)
     }
--- saucedemo/pages/login/controller.js
+++ saucedemo/pages/login/controller.js
@@ -1,7 +1,27 @@
+/* eslint-disable no-unused-vars */
+const { Page } = require("@playwright/test")
+/* eslint-enable no-unused-vars */
 const { LoginComponents, loginLocators } = require("./components")
 const { PAGES, CREDENTIALS } = require("../../utils/consts")
 
+/**
+ * Class representing the Login's page interations
+ */
 class LoginController {
+    /**
+     * Create the Login controller
+     * @param {Page} page - playwright browser's page\
+     * See {@link https://playwright.dev/docs/api/class-page}
+     * @param {LoginComponents} components - class with elementsHandle of the Login page
+     * @param {Object} locators - page's selectors
+     * @param {Object} standardUser - username of the standard user
+     * @param {Object} lockedUser - username of the locked user
+     * @param {Object} problemUser - username of the user that has some issues after login
+     * @param {Object} performanceGlitchUser - username of the user that has some performance
+     * issues after login
+     * @param {Object} acceptedUsers - list of usernames
+     * @param {Object} password - the password to login
+     */
     constructor(page) {
         this.page = page
         this.components = new LoginComponents(this.page)
@@ -14,10 +34,16 @@ class LoginController {
         this.password = CREDENTIALS.PASSWORD
     }
 
+    /**
+     * Navigate to the Login page and wait until network is idle
+     */
     async navigate() {
         await this.page.goto(`${PAGES.LOGIN}`, "networkidle")
     }
 
+    /**
+     * Validate if "loginContainer" and  "credentialsContainer" elements are visible
+     */
     async screenIsVisible() {
         const loginContainerElement = await this.components.loginContainer()
         const credentialsContainerElement = await this.components.credentialsContainer()
@@ -25,54 +51,92 @@ class LoginController {
         await credentialsContainerElement.isVisible()
     }
 
+    /**
+     * Fill up the username input
+     */
     async fillUserName(username) {
         const usernameElement = await this.components.usernameInput()
         await usernameElement.fill(username)
     }
 
+    /**
+     * Fill up the password input
+     */
     async fillPassword(password) {
         const passwordElement = await this.components.passwordInput()
         await passwordElement.fill(password)
     }
 
+    /**
+     * Click at the "Login" button
+     */
     async submitLogin() {
         const loginElement = await this.components.loginButton()
         await loginElement.click()
     }
 
+    /**
+     * Fill up username ans password inputs and click at the "Login" button
+     * @param {String} username - the username credential
+     * @param {String} password - the password credential
+     */
     async _login(username, password) {
         await this.fillUserName(username)
         await this.fillPassword(password)
         await this.submitLogin()
     }
 
+    /**
+     * Fill up username ans password inputs with the starndard credentials and click at the "Login" button
+     */
     async loginWithStandardUser() {
         await this._login(this.standardUser, this.password)
     }
 
+    /**
+     * Fill up username ans password inputs with the locked user credentials and click at the "Login" button
+     */
     async loginWithLockedUser() {
         await this._login(this.lockedUser, this.password)
     }
 
+    /**
+     * Fill up only the password input and click at the "Login" button
+     */
     async loginWithoutUser() {
         await this._login("", this.password)
     }
 
+    /**
+     * Fill up the username and the password input with a invalid user and click at the "Login" button
+     */
     async loginWithWrongCredential() {
         await this._login("noname", this.password)
     }
 
+    /**
+     * Get error message
+     * @returns {String} login's error message text
+     */
     async getErrorMessage() {
         const errorElement = await this.components.errorMessageText()
         return await errorElement.innerText()
     }
 
+    /**
+     * Get the list of accepted usernames
+     * @returns {String[]} the list of usernames from the credentials section
+     */
     async getAcceptedUsers() {
         let acceptedUsers = await this.components.acceptedUsersText()
         acceptedUsers = await acceptedUsers.innerText()
         return acceptedUsers.split("\n").filter(Boolean).slice(1)
     }
 
+    /**
+     * Get the system password
+     * @returns {String} the password text from the credentials section
+     */
     async getPassword() {
         let acceptedPassword = await this.components.acceptedPasswordText()
         acceptedPassword = await acceptedPassword.innerText()
