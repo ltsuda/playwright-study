d2ac9f6384e9a5c25cde4ec9b1e10f280e65128c
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-17 00:19:12 -0300

    update overview pom and tests to use the latest Locator class

--- saucedemo/pages/overview/components.js
+++ saucedemo/pages/overview/components.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-unused-vars */
-const { Page, ElementHandle } = require("@playwright/test")
+const { Page, Locator } = require("@playwright/test")
 /* eslint-enable no-unused-vars */
 
 /**
  * Object representing Overview's HTML selectors
  */
-const overviewLocators = {
+const overviewSelectors = {
     overviewContainer: ".checkout_summary_container",
     cancelCheckoutButton: "[data-test='cancel']",
     finishCheckoutButton: "[data-test='finish']",
-    summaryInfoContainer: "[class='summary_info']",
+    summaryInfoContainer: ".summary_info",
     paymentInfoText: "text=/saucecard #31337/i",
     shippingInfoText: "text=/FREE PONY EXPRESS DELIVERY!/i",
-    subtotalText: "[class='summary_subtotal_label']",
-    taxText: "[class='summary_tax_label']",
-    totalPriceText: "[class='summary_total_label']",
+    subtotalText: ".summary_subtotal_label",
+    taxText: ".summary_tax_label",
+    totalPriceText: ".summary_total_label",
 }
 
 /**
- * Class representing playwright's ElementHandles from /checkout-two.html page\
- * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ * Class representing playwright's Locators from /checkout-two.html page\
+ * See {@link https://playwright.dev/docs/api/class-locator}
  */
 class OverviewComponents {
     /**
-     * Create the Overview ElementsHandle
+     * Create the Overview Locators
      * @param {Page} page - playwright browser's page\
      * See {@link https://playwright.dev/docs/api/class-page}
      */
@@ -32,77 +32,79 @@ class OverviewComponents {
     }
 
     /**
-     * Get the overview's container ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'overviewContainer' locator
+     * Get the overview's container Locator
+     * @returns {Locator} Locator for 'overviewContainer' selector
      */
     async overviewContainer() {
-        return await this.page.$(overviewLocators.overviewContainer)
+        return await this.page.locator(overviewSelectors.overviewContainer)
     }
 
     /**
-     * Get the payment information text ElementHandle
-     * @returns {String} payment card element text
+     * Get the payment information text Locator
+     * @returns {String} payment card element text selector
      */
     async paymentInfoText() {
-        return await this.page.$(
-            `${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.paymentInfoText}`
+        return await this.page.locator(
+            `${overviewSelectors.summaryInfoContainer}` + ">>" + `${overviewSelectors.paymentInfoText}`
         )
     }
 
     /**
-     * Get the shipment information text ElementHandle
+     * Get the shipment information text Locator
      * @returns {String} shipment element text
      */
     async shippingInfoText() {
-        return await this.page.$(
-            `${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.shippingInfoText}`
+        return await this.page.locator(
+            `${overviewSelectors.summaryInfoContainer}` + ">>" + `${overviewSelectors.shippingInfoText}`
         )
     }
 
     /**
-     * Get the subtotal text ElementHandle
+     * Get the subtotal text Locator
      * @returns {String} subtotal element text
      */
     async subtotalText() {
-        return await this.page.$(`${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.subtotalText}`)
-    }
-
-    /**
-     * Get the tax text ElementHandle
-     * @returns {String} tax element text
-     */
-    async taxText() {
-        return await this.page.$(`${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.taxText}`)
-    }
-
-    /**
-     * Get the total price text ElementHandle
-     * @returns {String} total price element text
-     */
-    async totalPriceText() {
-        return await this.page.$(
-            `${overviewLocators.summaryInfoContainer}` + ">>" + `${overviewLocators.totalPriceText}`
+        return await this.page.locator(
+            `${overviewSelectors.summaryInfoContainer}` + ">>" + `${overviewSelectors.subtotalText}`
         )
     }
 
     /**
-     * Get the cancel checkout button ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'cancelCheckoutButton' locator
+     * Get the tax text Locator
+     * @returns {String} tax element text
      */
-    async cancelCheckoutButton() {
-        return await this.page.$(overviewLocators.cancelCheckoutButton)
+    async taxText() {
+        return await this.page.locator(`${overviewSelectors.summaryInfoContainer}` + ">>" + `${overviewSelectors.taxText}`)
     }
 
     /**
-     * Get the finish purchase button ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'finishCheckoutButton' locator
+     * Get the total price text Locator
+     * @returns {String} total price element text
+     */
+    async totalPriceText() {
+        return await this.page.locator(
+            `${overviewSelectors.summaryInfoContainer}` + ">>" + `${overviewSelectors.totalPriceText}`
+        )
+    }
+
+    /**
+     * Get the cancel checkout button Locator
+     * @returns {Locator} Locator for 'cancelCheckoutButton' selector
+     */
+    async cancelCheckoutButton() {
+        return await this.page.locator(overviewSelectors.cancelCheckoutButton)
+    }
+
+    /**
+     * Get the finish purchase button Locator
+     * @returns {Locator} Locator for 'finishCheckoutButton' selector
      */
     async finishCheckoutButton() {
-        return await this.page.$(overviewLocators.finishCheckoutButton)
+        return await this.page.locator(overviewSelectors.finishCheckoutButton)
     }
 }
 
 module.exports = {
     OverviewComponents,
-    overviewLocators,
+    overviewSelectors,
 }
