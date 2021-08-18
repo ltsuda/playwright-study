c9b8624f74a3a0fd78908c60388d80dfdd4aa6be
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-08-18 14:57:32 -0300

    update inventoryItem pom and tests to use the latest Locator class

--- saucedemo/pages/inventoryItem/components.js
+++ saucedemo/pages/inventoryItem/components.js
@@ -1,29 +1,30 @@
 /* eslint-disable no-unused-vars */
-const { Page, ElementHandle } = require("@playwright/test")
+const { Page, ElementHandle, Locator } = require("@playwright/test")
 /* eslint-enable no-unused-vars */
 
 /**
  * Object representing Inventory items' HTML selectors
  */
-const inventoryItemLocators = {
+const inventoryItemSelectors = {
     cartItemContainer: ".cart_item",
-    detailsItemContainer: "[class='inventory_details_container']",
-    itemContainer: "[class='inventory_item']",
+    detailsItemContainer: ".inventory_details_container",
+    itemContainer: ".inventory_item",
     itemNameText: ".inventory_item_name",
     itemDescriptionText: ".inventory_item_desc",
-    itemPriceText: "[class='inventory_item_price']",
+    itemPriceText: ".inventory_item_price",
     addToCartButton: "text=/add to cart/i",
     removeButton: "text=/remove/i",
     backToProductsButton: "[data-test='back-to-products']",
 }
 
 /**
- * Class representing playwright's ElementHandles for the inventory item elements\
- * See {@link https://playwright.dev/docs/api/class-elementhandle}
+ * Class representing playwright's ElementHandles/Locators for the inventory item elements\
+ * See {@link https://playwright.dev/docs/api/class-elementhandle}\
+ * See {@link https://playwright.dev/docs/api/class-locator}
  */
 class InventoryItemComponents {
     /**
-     * Create the Inventory item ElementsHandle
+     * Create the Inventory item ElementsHandle/Locators
      * @param {Page} page - playwright browser's page\
      * See {@link https://playwright.dev/docs/api/class-page}
      */
@@ -33,39 +34,39 @@ class InventoryItemComponents {
 
     /**
      * Select which container selector based on the page
-     * @returns {ElementHandle} ElementHandle for a container locator
+     * @returns {String} container locator
      */
     switchItemLocator(fromPage) {
         switch (fromPage) {
             case "cart":
-                return inventoryItemLocators.cartItemContainer
+                return inventoryItemSelectors.cartItemContainer
             case "inventory":
-                return inventoryItemLocators.itemContainer
+                return inventoryItemSelectors.itemContainer
             case "details":
-                return inventoryItemLocators.detailsItemContainer
+                return inventoryItemSelectors.detailsItemContainer
             default:
-                return inventoryItemLocators.itemContainer
+                return inventoryItemSelectors.itemContainer
         }
     }
 
     /**
-     * Get the inventory item detail's container ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'detailsItemContainer' locator
+     * Get the inventory item detail's container Locator
+     * @returns {Locator} Locator for 'detailsItemContainer' selector
      */
     async itemDetailContainer() {
-        return await this.page.$(inventoryItemLocators.detailsItemContainer)
+        return await this.page.locator(inventoryItemSelectors.detailsItemContainer)
     }
 
     /**
-     * Get the inventory item cart's container ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'cartItemContainer' locator
+     * Get the inventory item cart's container Locator
+     * @returns {Locator} Locator for 'cartItemContainer' selector
      */
     async cartItemContainer() {
-        return await this.page.$(inventoryItemLocators.cartItemContainer)
+        return await this.page.locator(inventoryItemSelectors.cartItemContainer)
     }
 
     /**
-     * Get a list of items container's ElementHandles
+     * Get a list of items container's ElementHandles/Locators
      * @returns {ElementHandle[]} a list of ElementHandle for the list of item's container locator
      */
     async items(fromPage = "inventory") {
@@ -74,59 +75,59 @@ class InventoryItemComponents {
     }
 
     /**
-     * Get a list of items name's ElementHandles
-     * @returns {ElementHandle[]} a list of ElementHandle for a 'itemNameText' locator
+     * Get a list of items name's ElementHandles/Locators
+     * @returns {ElementHandle[]} a list of ElementHandle for a 'itemNameText' selector
      */
     async itemsNameText(fromPage) {
         const locator = this.switchItemLocator(fromPage)
         const itemLocator =
             fromPage == "details"
-                ? inventoryItemLocators.itemNameText.replace("item", "details")
-                : inventoryItemLocators.itemNameText
+                ? inventoryItemSelectors.itemNameText.replace("item", "details")
+                : inventoryItemSelectors.itemNameText
         return await this.page.$$(`${locator}` + ">>" + `${itemLocator}`)
     }
 
     /**
-     * Get a list of items price's ElementHandles
-     * @returns {ElementHandle[]} a list of ElementHandle for a 'itemPriceText' locator
+     * Get a list of items price's ElementHandles/Locators
+     * @returns {ElementHandle[]} a list of ElementHandle for a 'itemPriceText' selector
      */
     async itemsPriceText(fromPage) {
         const locator = this.switchItemLocator(fromPage)
         const itemLocator =
             fromPage == "details"
-                ? inventoryItemLocators.itemPriceText.replace("_", "_details_")
-                : inventoryItemLocators.itemPriceText
+                ? inventoryItemSelectors.itemPriceText.replace("_", "_details_")
+                : inventoryItemSelectors.itemPriceText
         return await this.page.$$(`${locator}` + ">>" + `${itemLocator}`)
     }
 
     /**
-     * Get the add to cart button ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'addToCartButton' locator
+     * Get the add to cart button Locator
+     * @returns {Locator} Locator for 'addToCartButton' selector
      */
     async addToCartButton(fromPage) {
         const locator = this.switchItemLocator(fromPage)
-        return await this.page.$(`${locator}` + ">>" + `${inventoryItemLocators.addToCartButton}`)
+        return await this.page.locator(`${locator}` + ">>" + `${inventoryItemSelectors.addToCartButton}`)
     }
 
     /**
-     * Get the remove from cart button ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'removeButton' locator
+     * Get the remove from cart button Locator
+     * @returns {Locator} Locator for 'removeButton' selector
      */
     async removeItemsButton(fromPage) {
         const locator = this.switchItemLocator(fromPage)
-        return await this.page.$$(`${locator}` + ">>" + `${inventoryItemLocators.removeButton}`)
+        return await this.page.locator(`${locator}` + ">>" + `${inventoryItemSelectors.removeButton}`)
     }
 
     /**
-     * Get the back to products button ElementHandle
-     * @returns {ElementHandle} ElementHandle for 'backToProductsButton' locator
+     * Get the back to products button Locator
+     * @returns {Locator} Locator for 'backToProductsButton' selector
      */
     async backToProductsButton() {
-        return await this.page.$(inventoryItemLocators.backToProductsButton)
+        return await this.page.locator(inventoryItemSelectors.backToProductsButton)
     }
 }
 
 module.exports = {
     InventoryItemComponents,
-    inventoryItemLocators,
+    inventoryItemSelectors,
 }
