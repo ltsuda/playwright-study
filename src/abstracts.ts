5d0ad864e38f1487eca6636fb5206f0d674b4225
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-05-18 15:27:56 -0300

    chore: add abstracts link reference

--- src/abstracts.ts
+++ src/abstracts.ts
@@ -1,5 +1,7 @@
 import { type Locator, type Page as PlaywrightPage } from '@playwright/test';
 
+// reference idea from: https://github.com/microsoft/playwright/issues/1604#issuecomment-1004711489
+
 export abstract class ComponentObject {
   constructor(public readonly host: Locator) { }
 }
