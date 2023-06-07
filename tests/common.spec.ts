7ab5869c34ea7b715797c9456d53627623c52353
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-06-07 02:14:09 -0300

    chore: add exclusive lint-staged script and run

--- tests/common.spec.ts
+++ tests/common.spec.ts
@@ -1,40 +1,40 @@
-import { Footer } from "../src/components/footer"
-import { PrimaryHeader } from "../src/components/primary-header"
-import { SecondaryHeader } from "../src/components/secondary-header"
-import { MESSAGES, SECONDARY_HEADER_BY_PAGE, SOCIAL_LINKS } from "../src/consts"
-import { expect, test } from "../src/fixtures"
+import { Footer } from '../src/components/footer';
+import { PrimaryHeader } from '../src/components/primary-header';
+import { SecondaryHeader } from '../src/components/secondary-header';
+import { MESSAGES, SECONDARY_HEADER_BY_PAGE, SOCIAL_LINKS } from '../src/consts';
+import { expect, test } from '../src/fixtures';
 
-test.use({ storageState: "storage.json" })
+test.use({ storageState: 'storage.json' });
 for (const item of Object.values(SECONDARY_HEADER_BY_PAGE)) {
+  test(`headers and foot elements at ${item.PAGE} page`, async ({ page }) => {
+    const headerContainerLocator = page.getByTestId('header-container');
+    const primaryHeaderLocator = headerContainerLocator.getByTestId('header-primary');
+    const secondaryHeaderLocator = headerContainerLocator.getByTestId('header-secondary');
+    const primaryHeader = new PrimaryHeader(primaryHeaderLocator);
+    const secondaryHeader = new SecondaryHeader(secondaryHeaderLocator);
+    const footer = new Footer(page.getByTestId('footer'));
 
-    test(`headers and foot elements at ${item.PAGE} page`, async ({ page }) => {
-        const headerContainerLocator = page.getByTestId("header-container")
-        const primaryHeaderLocator = headerContainerLocator.getByTestId("header-primary")
-        const secondaryHeaderLocator = headerContainerLocator.getByTestId("header-secondary")
-        const primaryHeader = new PrimaryHeader(primaryHeaderLocator)
-        const secondaryHeader = new SecondaryHeader(secondaryHeaderLocator)
-        const footer = new Footer(page.getByTestId("footer"))
+    await page.goto(`.${item.PAGE}`);
+    await expect(page).toHaveURL(new RegExp(`.*${item.PAGE}`));
 
-        await page.goto(`.${item.PAGE}`)
-        expect(page).toHaveURL(new RegExp(`.*${item.PAGE}`))
+    // Primary Header
+    await expect.soft(primaryHeader.host).toBeVisible();
+    await expect.soft(primaryHeader.sideMenu.openButton).toBeVisible();
+    await expect.soft(primaryHeader.cart.cartLink).toBeVisible();
+    await expect.soft(primaryHeader.appLogo).toBeVisible();
 
-        // Primary Header
-        await expect.soft(primaryHeader.host).toBeVisible()
-        await expect.soft(primaryHeader.sideMenu.openButton).toBeVisible()
-        await expect.soft(primaryHeader.cart.cartLink).toBeVisible()
-        await expect.soft(primaryHeader.appLogo).toBeVisible()
+    // Secondary Header
+    // eslint-disable-next-line playwright/no-conditional-in-test
+    if (page.url().match(/.*inventory-item.*/)) {
+      await expect.soft(secondaryHeader.backToProductsButton).toHaveText(item.TITLE);
+    } else {
+      await expect.soft(secondaryHeader.title).toHaveText(item.TITLE);
+    }
 
-        // Secondary Header
-        if (page.url().match(/.*inventory-item.*/)) {
-            await expect.soft(secondaryHeader.backToProductsButton).toHaveText(item.TITLE)
-        } else {
-            await expect.soft(secondaryHeader.title).toHaveText(item.TITLE)
-        }
-
-        // Footer
-        await expect.soft(footer.twitterLink).toHaveAttribute("href", SOCIAL_LINKS.TWITTER)
-        await expect.soft(footer.facebookLink).toHaveAttribute("href", SOCIAL_LINKS.FACEBOOK)
-        await expect.soft(footer.linkedInLink).toHaveAttribute("href", SOCIAL_LINKS.LINKEDIN)
-        await expect.soft(footer.copyrightText).toHaveText(MESSAGES.COPYRIGHT)
-    })
+    // Footer
+    await expect.soft(footer.twitterLink).toHaveAttribute('href', SOCIAL_LINKS.TWITTER);
+    await expect.soft(footer.facebookLink).toHaveAttribute('href', SOCIAL_LINKS.FACEBOOK);
+    await expect.soft(footer.linkedInLink).toHaveAttribute('href', SOCIAL_LINKS.LINKEDIN);
+    await expect.soft(footer.copyrightText).toHaveText(MESSAGES.COPYRIGHT);
+  });
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,46 +1,45 @@
-import { CREDENTIALS, ERRORS, PAGES } from "../src/consts"
-import { expect, test } from "../src/fixtures"
+import { CREDENTIALS, ERRORS, PAGES } from '../src/consts';
+import { expect, test } from '../src/fixtures';
 
-test.describe("Login Suite: @login", () => {
-    test.beforeEach(async ({ loginPage, baseURL }) => {
-        await loginPage.page.goto(baseURL ?? "./")
-    })
-    test("should be at the login page", async ({ loginPage, baseURL }) => {
-        expect(loginPage.page.url()).toEqual(`${baseURL}/`)
-    })
-    test("should show accepted users", async ({ loginPage }) => {
-        const users = await loginPage.credentialsContainer.getAvailableUsernames()
-        const expectedUsers = Object.values(CREDENTIALS.USERS)
-        expect(users).toEqual(expectedUsers)
-    })
-    test("should show application's password ", async ({ loginPage }) => {
-        const passwords = await loginPage.credentialsContainer.getAvailablePasswords()
-        const expectedPasswords = new Array(CREDENTIALS.PASSWORD)
-        expect(passwords).toEqual(expectedPasswords)
-    })
+test.describe('Login Suite: @login', () => {
+  test.beforeEach(async ({ loginPage, baseURL }) => {
+    await loginPage.page.goto(baseURL ?? './');
+  });
+  test('should be at the login page', async ({ loginPage, baseURL }) => {
+    expect(loginPage.page.url()).toEqual(`${baseURL}/`);
+  });
+  test('should show accepted users', async ({ loginPage }) => {
+    const users = await loginPage.credentialsContainer.getAvailableUsernames();
+    const expectedUsers = Object.values(CREDENTIALS.USERS);
+    expect(users).toEqual(expectedUsers);
+  });
+  test("should show application's password ", async ({ loginPage }) => {
+    const passwords = await loginPage.credentialsContainer.getAvailablePasswords();
+    const expectedPasswords = new Array(CREDENTIALS.PASSWORD);
+    expect(passwords).toEqual(expectedPasswords);
+  });
 
-    test("should show locked user error", async ({ loginPage }) => {
-        await loginPage.loginContainer.usernameInput.fill(CREDENTIALS.USERS.LOCKED)
-        await loginPage.loginContainer.passwordInput.fill(CREDENTIALS.PASSWORD)
-        await loginPage.loginContainer.loginButton.click()
-        await expect(loginPage.errorContainer.host).toBeVisible()
-        await expect(loginPage.errorContainer.errorMessage).toHaveText(ERRORS.LOGIN_LOCKED)
-    })
-    test("should show username is required error", async ({ loginPage }) => {
-        await loginPage.loginContainer.loginButton.click()
-        await expect(loginPage.errorContainer.host).toBeVisible()
-        await expect(loginPage.errorContainer.errorMessage).toHaveText(ERRORS.LOGIN_USER)
-    })
-    test("should show username and password doesn't match", async ({ loginPage }) => {
-        await loginPage.loginContainer.usernameInput.fill(CREDENTIALS.USERS.LOCKED)
-        await loginPage.loginContainer.passwordInput.fill("invalid")
-        await loginPage.loginContainer.loginButton.click()
-        await expect(loginPage.errorContainer.host).toBeVisible()
-        await expect(loginPage.errorContainer.errorMessage).toHaveText(ERRORS.LOGIN_CREDENTIALS)
-
-    })
-    test("should navigate to inventory page after successful login", async ({ loginPage, inventoryPage }) => {
-        await loginPage.loginWithUser(CREDENTIALS.USERS.STANDARD, CREDENTIALS.PASSWORD)
-        await expect(inventoryPage.page).toHaveURL(PAGES.INVENTORY)
-    })
-})
+  test('should show locked user error', async ({ loginPage }) => {
+    await loginPage.loginContainer.usernameInput.fill(CREDENTIALS.USERS.LOCKED);
+    await loginPage.loginContainer.passwordInput.fill(CREDENTIALS.PASSWORD);
+    await loginPage.loginContainer.loginButton.click();
+    await expect(loginPage.errorContainer.host).toBeVisible();
+    await expect(loginPage.errorContainer.errorMessage).toHaveText(ERRORS.LOGIN_LOCKED);
+  });
+  test('should show username is required error', async ({ loginPage }) => {
+    await loginPage.loginContainer.loginButton.click();
+    await expect(loginPage.errorContainer.host).toBeVisible();
+    await expect(loginPage.errorContainer.errorMessage).toHaveText(ERRORS.LOGIN_USER);
+  });
+  test("should show username and password doesn't match", async ({ loginPage }) => {
+    await loginPage.loginContainer.usernameInput.fill(CREDENTIALS.USERS.LOCKED);
+    await loginPage.loginContainer.passwordInput.fill('invalid');
+    await loginPage.loginContainer.loginButton.click();
+    await expect(loginPage.errorContainer.host).toBeVisible();
+    await expect(loginPage.errorContainer.errorMessage).toHaveText(ERRORS.LOGIN_CREDENTIALS);
+  });
+  test('should navigate to inventory page after successful login', async ({ loginPage, inventoryPage }) => {
+    await loginPage.loginWithUser(CREDENTIALS.USERS.STANDARD, CREDENTIALS.PASSWORD);
+    await expect(inventoryPage.page).toHaveURL(PAGES.INVENTORY);
+  });
+});
