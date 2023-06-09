5c8a9155bdd5ea71d4338548de436f2b1eb950b2
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-06-08 23:42:33 -0300

    test: add side menu specs

--- src/components/primary-header.ts
+++ src/components/primary-header.ts
@@ -6,5 +6,10 @@ import { SideMenu } from './side-menu';
 export class PrimaryHeader extends ComponentObject {
   public readonly appLogo: Locator = this.host.locator('.app_logo');
   public readonly cart: CartContainer = new CartContainer(this.host.locator('.shopping_cart_container'));
-  public readonly sideMenu: SideMenu = new SideMenu(this.host.getByTestId('menu-drawer'));
+  public readonly sideMenu: SideMenu = new SideMenu(this.host.locator('.bm-menu-wrap'));
+  public readonly openSideMenuButton: Locator = this.host.getByRole('button', { name: 'Open Menu' });
+
+  public async openSideMenu() {
+    await this.openSideMenuButton.click();
+  }
 }
--- tests/side-menu.spec.ts
+++ tests/side-menu.spec.ts
@@ -1,22 +1,38 @@
+import { setSession } from '../src/common';
 import { expect, test } from '../src/fixtures';
+import { CREDENTIALS, PAGES } from '../src/consts';
 
 test.describe('Side Menu Suite: @side-menu', () => {
-  test('should open side menu', async () => {
-    expect(true).toBeFalsy();
+  test('should open side menu', async ({ cartPage }) => {
+    await setSession(cartPage, { path: PAGES.CHECKOUT, username: CREDENTIALS.USERS.STANDARD });
+    await cartPage.primaryHeader.openSideMenu();
+    await expect(cartPage.primaryHeader.sideMenu.host).toBeVisible();
   });
-  test('should close side menu', async () => {
-    expect(true).toBeFalsy();
+  test('should close side menu', async ({ cartPage }) => {
+    await setSession(cartPage, { path: PAGES.CHECKOUT, username: CREDENTIALS.USERS.STANDARD });
+    await cartPage.primaryHeader.openSideMenu();
+    await cartPage.primaryHeader.sideMenu.close();
+    await expect(cartPage.primaryHeader.sideMenu.host).toBeHidden();
   });
-  test('should navigate back to inventory list page', async () => {
-    expect(true).toBeFalsy();
+  test('should navigate back to inventory list page', async ({ baseURL, cartPage, inventoryPage }) => {
+    await setSession(cartPage, { path: PAGES.CHECKOUT, username: CREDENTIALS.USERS.STANDARD });
+    await cartPage.primaryHeader.openSideMenu();
+    await cartPage.primaryHeader.sideMenu.allItemLink.click();
+    await expect(inventoryPage.page).toHaveURL(`${baseURL}${PAGES.INVENTORY}`);
   });
-  test('should have saucelabs link in about menu item', async () => {
-    expect(true).toBeFalsy();
+  test('should have saucelabs link in about menu item', async ({ cartPage }) => {
+    await setSession(cartPage, { path: PAGES.CHECKOUT, username: CREDENTIALS.USERS.STANDARD });
+    await cartPage.primaryHeader.openSideMenu();
+    await expect(cartPage.primaryHeader.sideMenu.aboutLink).toHaveAttribute('href', 'https://saucelabs.com/');
   });
-  test('should logout from application', async () => {
-    expect(true).toBeFalsy();
+  test('should logout from application', async ({ baseURL, cartPage, loginPage }) => {
+    await setSession(cartPage, { path: PAGES.CHECKOUT, username: CREDENTIALS.USERS.STANDARD });
+    await cartPage.primaryHeader.openSideMenu();
+    await cartPage.primaryHeader.sideMenu.logout();
+    await expect(loginPage.page).toHaveURL(`${baseURL}/`);
   });
-  test('should reset cart badge', async () => {
+  test('should reset cart badge', async ({ cartPage }) => {
+    await setSession(cartPage, { path: PAGES.CHECKOUT, username: CREDENTIALS.USERS.STANDARD });
     expect(true).toBeFalsy();
   });
 });
