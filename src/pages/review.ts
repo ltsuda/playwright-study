3781a600eb31f302f52d8d9d39d3947de56b979a
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-06-09 14:11:41 -0300

    test: add review purchase specs

--- src/pages/review.ts
+++ src/pages/review.ts
@@ -1,3 +1,12 @@
 import { Page } from '../abstracts';
 
-export class ReviewPage extends Page {}
+export class ReviewPage extends Page {
+  public readonly cancelButton = this.page.getByTestId('button-cancel');
+  public readonly paymentLabel = this.page.getByTestId('label-payment-information');
+  public readonly paymentValue = this.page.getByTestId('value-payment-information');
+  public readonly shippingLabel = this.page.getByTestId('label-shipping-information');
+  public readonly shippingValue = this.page.getByTestId('value-shipping-information');
+  public readonly subtotalValue = this.page.getByTestId('value-subtotal');
+  public readonly taxValue = this.page.getByTestId('label-tax');
+  public readonly totalValue = this.page.getByTestId('label-total');
+}
--- tests/review.spec.ts
+++ tests/review.spec.ts
@@ -1,28 +1,49 @@
+import { setSession } from '../src/common';
+import { CREDENTIALS, PAGES, PRODUCTS_NAMES } from '../src/consts';
 import { expect, test } from '../src/fixtures';
 
-test.describe.fixme('Review Purchase Suite: @review', () => {
-  test('should be at review item page url', async () => {
+test.describe('Review Purchase Suite: @review', () => {
+  const products = [PRODUCTS_NAMES.ALL_TSHIRT.index, PRODUCTS_NAMES.BOLT_TSHIRT.index].reduce(
+    (productsIndexAsInt: number[], productIndex) => {
+      productsIndexAsInt.push(parseInt(productIndex, 10));
+      return productsIndexAsInt;
+    },
+    []
+  );
+
+  test('should be at review item page url', async ({ baseURL, reviewPage }) => {
+    await setSession(reviewPage, { path: PAGES.OVERVIEW, username: CREDENTIALS.USERS.STANDARD });
+    await expect(reviewPage.page).toHaveURL(`${baseURL}${PAGES.OVERVIEW}`);
+  });
+  test.fixme("should have items' details", async () => {
     expect(true).toBeFalsy();
   });
-  test("should have items' details", async () => {
+  test('should have payment information', async ({ reviewPage }) => {
+    await setSession(reviewPage, { path: PAGES.OVERVIEW, username: CREDENTIALS.USERS.STANDARD });
+    await expect(reviewPage.paymentLabel).toHaveText(/Payment Information/);
+    await expect(reviewPage.paymentValue).toHaveText('SauceCard #31337');
+  });
+  test('should have shipping information', async ({ reviewPage }) => {
+    await setSession(reviewPage, { path: PAGES.OVERVIEW, username: CREDENTIALS.USERS.STANDARD });
+    await expect(reviewPage.shippingLabel).toHaveText(/Shipping Information/);
+    await expect(reviewPage.shippingValue).toHaveText('Free Pony Express Delivery!');
+  });
+  test.fixme('should have sum of item prices', async ({ reviewPage }) => {
+    await setSession(reviewPage, {
+      path: PAGES.OVERVIEW,
+      username: CREDENTIALS.USERS.STANDARD,
+      productsIndex: products,
+    });
+  });
+  test.fixme('should have tax value (8%) item total', async () => {
     expect(true).toBeFalsy();
   });
-  test('should have payment information', async () => {
+  test.fixme('should have total value with tax', async () => {
     expect(true).toBeFalsy();
   });
-  test('should have shipping information', async () => {
-    expect(true).toBeFalsy();
-  });
-  test('should have sum of item prices', async () => {
-    expect(true).toBeFalsy();
-  });
-  test('should have tax value (8%) item total', async () => {
-    expect(true).toBeFalsy();
-  });
-  test('should have total value with tax', async () => {
-    expect(true).toBeFalsy();
-  });
-  test('should be at inventory page if cancel purchase', async () => {
-    expect(true).toBeFalsy();
+  test('should be at inventory page if cancel purchase', async ({ baseURL, reviewPage, inventoryPage }) => {
+    await setSession(reviewPage, { path: PAGES.OVERVIEW, username: CREDENTIALS.USERS.STANDARD });
+    await reviewPage.cancelButton.click();
+    await expect(inventoryPage.page).toHaveURL(`${baseURL}${PAGES.INVENTORY}`);
   });
 });
