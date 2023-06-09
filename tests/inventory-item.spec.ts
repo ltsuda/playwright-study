c26d077d7434b1aa6e919907349d1f28e1cb900a
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-06-09 00:58:17 -0300

    test: add inventory item specs and refactor setSession

--- tests/inventory-item.spec.ts
+++ tests/inventory-item.spec.ts
@@ -1,11 +1,21 @@
+import { setSession } from '../src/common';
+import { CREDENTIALS, PAGES } from '../src/consts';
 import { expect, test } from '../src/fixtures';
 
 test.describe('Inventory Item Suite: @inventory-item', () => {
-  test('should be at inventory item page url', async () => {
-    expect(true).toBeFalsy();
+  let randomItemId: number;
+
+  test.beforeEach(async () => {
+    randomItemId = Math.floor(Math.random() * 6);
   });
-  test('should have item id at url', async () => {
-    expect(true).toBeFalsy();
+
+  test('should have item id at url', async ({ baseURL, inventoryItemPage }) => {
+    await setSession(inventoryItemPage, {
+      path: PAGES.INVENTORY_ITEM,
+      username: CREDENTIALS.USERS.STANDARD,
+      productsIndex: [randomItemId],
+    });
+    await expect(inventoryItemPage.page).toHaveURL(`${baseURL}${PAGES.INVENTORY_ITEM}?id=${randomItemId}`);
   });
   test('should have item details', async () => {
     expect(true).toBeFalsy();
@@ -16,7 +26,17 @@ test.describe('Inventory Item Suite: @inventory-item', () => {
   test('should remove item from cart', async () => {
     expect(true).toBeFalsy();
   });
-  test('should be at inventory page if click at back to products', async () => {
-    expect(true).toBeFalsy();
+  test('should be at inventory page if click at back to products', async ({
+    baseURL,
+    inventoryPage,
+    inventoryItemPage,
+  }) => {
+    await setSession(inventoryItemPage, {
+      path: PAGES.INVENTORY_ITEM,
+      username: CREDENTIALS.USERS.STANDARD,
+      productsIndex: [randomItemId],
+    });
+    await inventoryItemPage.secondaryHeader.backToProductsButton.click();
+    await expect(inventoryPage.page).toHaveURL(`${baseURL}${PAGES.INVENTORY}`);
   });
 });
